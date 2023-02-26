f32872c40286de36ad024baf608fbc348cf5d88a
eemottus <emottus@tktk.eegit config --global user.name eemottus git config --global user.email emottus@tktk.ee>
2023-02-26 10:00:33 +0200

    week8

---
+++ Week8EpicGame/Program.cs
@@ -0,0 +1,27 @@
+﻿string folderPath = @"E:\MicrosoftVisualStudio\ProgrammerimiseAlgkursus\Tööd\DATA\";
+string heroFile = "heroes.txt";
+string villainFile = "villains.txt";
+
+string[] heroes = File.ReadAllLines(Path.Combine(folderPath, heroFile));
+string[] villains = File.ReadAllLines(Path.Combine(folderPath, villainFile));
+
+//string[] heroes = { "Harry potter", "Luke Skywalker", "Lara Croft", "Scooby-Doo" };
+//string[] villians = { "Voldemort", "Darthvade", "Dracula", "Joker", "Sauron" };
+string[] weapon = { "Magic wand", "Fork", "Banana", "Wooden sword", "Brick" };
+
+string hero = GetRandomValueFromArray(heroes);
+string heroWeapon = GetRandomValueFromArray(weapon);
+Console.WriteLine($"Today {hero} with {heroWeapon} saves the day!");
+
+string villain = GetRandomValueFromArray(villians);
+string villainWeapon = GetRandomValueFromArray(weapon);
+Console.WriteLine($"{villain} with {villainWeapon} tries to take over the world!");
+
+
+static string GetRandomValueFromArray(string[] someArray)
+{
+    Random rnd = new Random();
+    int randomIndex = rnd.Next(0, someArray.Length );
+    string randomStringFromArray = someArray[randomIndex];
+    return randomStringFromArray;
+}
